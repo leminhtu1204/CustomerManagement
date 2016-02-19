29f19cf4fd0f5362e60800e80d2ebf0414337533
leminhtu1204 <leminhtu12041988@gmail.com>
2016-02-19 22:35:33 +0700

    Tu

---
+++ CustomerManager/DataAccess/Repository/IStateRepository.cs
@@ -0,0 +1,9 @@
+﻿using CustomerManager.Model;
+using DataAccess.Entity;
+
+namespace DataAccess.Repository
+{
+    public interface IStateRepository : IRepository<State>
+    {
+    }
+}
---
+++ CustomerManager/DataAccess/Repository/StateRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using CustomerManager.Model;
+using DataAccess.Entity;
+
+namespace DataAccess.Repository
+{
+    public class StateRepository : Repository<State>, IStateRepository
+    {
+       
+    }
+}
